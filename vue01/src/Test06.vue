398fb3124db98b94db94479a204687e829b992c2
Ben Chen <idben.chen@gmail.com>
2026-08-12 12:02:16 +0800

    computed 的進階用法 (setter/getter)

---
+++ vue01/src/Test06.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const firstName = ref('John')
+const lastName = ref('Doe')
+
+// computed 的唯讀寫法
+// const fullName = computed(() => `${firstName.value} ${lastName.value}`)
+
+const fullName = computed({
+    get() {
+        return `${firstName.value} ${lastName.value}`
+    },
+    set(newValue) {
+        console.log(newValue);
+        const nameAry = newValue.split(' ')
+        firstName.value = nameAry[0] || ''
+        lastName.value = nameAry[1] || ''
+    }
+})
+</script>
+
+<template>
+    <h1>computed 的進階用法 (setter/getter)</h1>
+    <div>名: <input type="text" v-model="firstName"></div>
+    <div>姓: <input type="text" v-model="lastName"></div>
+    <div>全名: <input type="text" v-model="fullName"></div>
+    <p>目前的全名: {{ fullName }}</p>
+</template>
+
+<style scoped></style>
--- vue01/src/main.js
+++ vue01/src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './Pages.vue'
+import App from './Test06.vue'
 
 createApp(App).mount('#app')
